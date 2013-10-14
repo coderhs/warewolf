370acad214b9c547ca04ee82c476d77dab44db8a
coderhs <mailme@hsps.in>
2013-10-14 11:29:43 +0530

    Proper ruby coding etiquette

--- odp.rb
+++ odp.rb
@@ -1,26 +1,23 @@
-require 'rubygems'
 require 'zip/zip'
  
-class Decomp
+class Odp
 # unziping of the odp files to read the xml data of the presentation
 
-def initialize(file,destination)
-	@file = file
-	@destination = destination
+  def initialize(file,destination)
+    @file = file
+    @destination = destination
+  end
+
+  def unzip
+    Zip::ZipFile.open(@file) { |zip_file|
+      zip_file.each { |f|
+        f_path=File.join(@destination, f.name)
+        FileUtils.mkdir_p(File.dirname(f_path))
+        zip_file.extract(f, f_path) unless File.exist?(f_path)
+      }
+    }
+  end
 end
 
-def self.unzip_odp ()
-  Zip::ZipFile.open(@file) { |zip_file|
-   zip_file.each { |f|
-     f_path=File.join(@destination, f.name)
-     FileUtils.mkdir_p(File.dirname(f_path))
-     zip_file.extract(f, f_path) unless File.exist?(f_path)
-   }
-  }
-end
+Odp.new(ARGV[0],ARGV[1]).unzip
 
-end
-
-
-Decomp.new(ARGV[0],ARGV[1])
-Decomp.unzip_odp()
